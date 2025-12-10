4d16bf02d4679043819ae27ce791992bd17ec4e9
Ahmed Imran <ahmedimrankhaan@gmail.com>
2025-12-10 09:48:41 +0500

    Seed avatars in tables

--- src/database/seeder/avatar.seeder.ts
+++ src/database/seeder/avatar.seeder.ts
@@ -12,16 +12,21 @@ export class AvatarSeeder {
   async run() {
     console.log('➡ Seeding Avatars...');
     const data = [
-      { id: 1, name: 'Busy Pilates Mom', thumbnail: 'upload/avatars/pilates_mom.png' },
-      { id: 2, name: 'Older Pilates Woman', thumbnail: 'upload/avatars/older_woman.png' },
-      { id: 3, name: 'No Avatar', thumbnail: 'upload/avatars/no_avatar.png' },
-      { id: 4, name: 'Yoga Enthusiast', thumbnail: 'upload/avatars/yoga_enthusiast.png' },
-      { id: 5, name: 'Young Athlete', thumbnail: 'upload/avatars/young_athlete.png' },
-      { id: 6, name: 'Casual Jogger', thumbnail: 'upload/avatars/casual_jogger.png' },
-      { id: 7, name: 'Fitness Coach', thumbnail: 'upload/avatars/fitness_coach.png' },
-      { id: 8, name: 'Meditating Woman', thumbnail: 'upload/avatars/meditating_woman.png' },
-      { id: 9, name: 'Gym Trainer', thumbnail: 'upload/avatars/gym_trainer.png' },
-      { id: 10, name: 'Happy Mom', thumbnail: 'upload/avatars/happy_mom.png' },
+      { id: 1, name: 'Denver', thumbnail: 'upload/avatars/Rectangle 28020.png' },
+      { id: 2, name: 'Xena', thumbnail: 'upload/avatars/Rectangle 28021.png' },
+      { id: 3, name: 'Bear', thumbnail: 'upload/avatars/Rectangle 28029.png' },
+      { id: 4, name: 'Vera', thumbnail: 'upload/avatars/Rectangle 28019.png' },
+      { id: 5, name: 'Beckham', thumbnail: 'upload/avatars/Rectangle 28032.png' },
+      { id: 6, name: 'Bree', thumbnail: 'upload/avatars/Rectangle 28028.png' },
+      { id: 7, name: 'Hank', thumbnail: 'upload/avatars/Rectangle 28033.png' },
+      { id: 8, name: 'Teagan', thumbnail: 'upload/avatars/Rectangle 28031.png' },
+      { id: 9, name: 'Hank', thumbnail: 'upload/avatars/Rectangle 28030.png' },
+      { id: 10, name: 'Xena', thumbnail: 'upload/avatars/Rectangle 28022.png' },
+      { id: 11, name: 'Otto', thumbnail: 'upload/avatars/Rectangle 28023.png' },
+      { id: 12, name: 'Frida', thumbnail: 'upload/avatars/Rectangle 28024.png' },
+      { id: 13, name: 'Parker', thumbnail: 'upload/avatars/Rectangle 28025.png' },
+      { id: 14, name: 'Ivy', thumbnail: 'upload/avatars/Rectangle 28026.png' },
+      { id: 15, name: 'Dylan', thumbnail: 'upload/avatars/Rectangle 28030.png' },
     ];
  
     for (const item of data) {
